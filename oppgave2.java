6fc5ab503c371bff39f4c4232c1262999193e345
Sebastian K. B <43097835+Sebbeian@users.noreply.github.com>
2020-09-17 15:13:21 +0200

    Add files via upload

---
+++ oppgave2.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Comperator;
+import java.util.List;
+
+
+public class oppgave2 {
+	
+	public static void main(String[] args) {
+		
+		private static void lonnsoppgjor(List<Ansatt> ansatte,)
+		
+		
+		private static void skrivUtAlle(List<Ansatt> ansatte)
+		
+	}
+}
+
+public class Ansatt implements Runnable {
+	
+	public static <T, U extends Comparable<? super U>> Comparator<T> comparing(
+			Function<? super T>)
+	
+	List<Ansatt> empList = new ArrayList<>();
+	empList.add(new Ansatt("Henrik", "Olsen", "Mann", "CEO", 1 000 000))
+	empList.add(new Ansatt("Karl", "Olafsen", "Mann", "CFO", 900 000))
+	empList.add(new Ansatt("Guri", "Hansen", "Kvinne", "CTO", 900 000))
+	empList.add(new Ansatt("Trond", "Larsen", "Mann", "Senior Utvikler",  550 000))
+	empList.add(new Ansatt("Line", "Fyllingen", "Kvinne", "Junior Utvikler ", 450 000))
+	
+	Comparator<Ansatt> comp = Comparator.comparing()
+
+}
+
+public class Kjonn implements Runnable {
+
+}
