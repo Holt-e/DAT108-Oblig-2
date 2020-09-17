9fcadce9bfbf9c2297593a976f0a6eb74d268fd2
Sebastian K. B <43097835+Sebbeian@users.noreply.github.com>
2020-09-17 15:14:52 +0200

    Add files via upload

---
+++ Oppgave1a.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Oppgave1 {
+	
+	public static void main(String[] args) {
+		
+	
+	List<String> list = Arrays.asList("10", "1", "20", "110", "21", "12");
+	
+	Collections.sort(list);
+	System.out.println(list);
+	
+	}
+}
